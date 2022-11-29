f4a021290b8c9a7aaeece450b579d3b95c14c6dd
Laura.Foxey <roura_sasunaru@hotmail.com>
2022-11-29 04:56:52 +0000

    add radio button

--- src/Searchbar.tsx
+++ src/Searchbar.tsx
@@ -4,7 +4,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import { AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai';
 import LiveSearch from "./LiveSearch";
 import { DatePicker, DateRangePicker, DateRangePickerValue  } from '@mantine/dates';
-import { Button, RangeSlider} from '@mantine/core';
+import { Button, RangeSlider, MantineProvider, SegmentedControl } from '@mantine/core';
 
 
 function Searchbar() {
@@ -13,7 +13,7 @@ function Searchbar() {
     new Date(),
   ]);
   const [day, setDay] = useState<Date | null>(new Date());
-  const [ret, setRet] = useState(true);
+  const [ret, setRet] = useState('return');
   const [origin, setOrigin] = useState('');
   const [destination, setDestination] = useState('');
   const [adults, setAdults] = useState(1);
@@ -48,51 +48,67 @@ function Searchbar() {
   }
 
   return (
-    <div className="search">
-      <div className="search__traveltype">
-        <Button variant="gradient" gradient={{ from: 'grape', to: 'violet' }} onClick={() => setRet(false)} > One way </Button>
-        <Button variant="gradient" gradient={{ from: 'grape', to: 'violet' }} onClick={() => setRet(true)}> Return  </Button>
-      </div>
+    <MantineProvider
+      theme={{
+        colors: {
+          'white': ['#ffffff', '#e5e5e5'],
+        },
+      }}
+    >
+      <div className="search">
+        <div className="search__traveltype">
+          <SegmentedControl
+            size="lg"
+            color="grape"
+            value={ret}
+            onChange={setRet}
+            data={[
+              { label: 'One-way', value: 'one' },
+              { label: 'Return', value: 'return' },
+            ]}
+            />
+        </div>
 
-      <div className="search__box">
-        <LiveSearch placeholder={"Origin"} data={values} datavalue={origin} setValue={setOrigin} />
-        <LiveSearch
-          placeholder={"Destination"} data={values.filter(d => d.value !== origin)} 
-          datavalue={destination} setValue={setDestination} /> 
-      </div>
+        <div className="search__box">
+          <LiveSearch placeholder={"Origin"} data={values} datavalue={origin} setValue={setOrigin} />
+          <LiveSearch
+            placeholder={"Destination"} data={values.filter(d => d.value !== origin)} 
+            datavalue={destination} setValue={setDestination} /> 
+        </div>
 
-      <div className="search__box">
-       { ret ?  <DateRangePicker
-          label="Select dates"
-          minDate={new Date()}
-          value={date}
-          onChange={setDate}
-          clearable={false}
-        /> : <DatePicker label="Select date" minDate={new Date()} value={day} onChange={setDay} clearable={false}/> }
-        
-      </div>
+        <div className="search__box">
+        { ret==='return' ?  <DateRangePicker
+            label="Select dates"
+            minDate={new Date()}
+            value={date}
+            onChange={setDate}
+            clearable={false}
+          /> : <DatePicker label="Select date" minDate={new Date()} value={day} onChange={setDay} clearable={false}/> }
+          
+        </div>
 
-      <div className="search__passengers">
-        <p> Passengers: </p>
-        <section className="search__passengers__section">
-          <AiFillMinusCircle style={{fontSize: "180%"}} onClick={() => decrementLimit("adult")}/>
-          <p> {adults} Adults(12+) </p>
-          <AiFillPlusCircle style={{fontSize: "180%"}} onClick={() => setAdults(prev => prev + 1)}/>
-        </section>
-        <section className="search__passengers__section">
-          <AiFillMinusCircle style={{fontSize: "180%"}} onClick={() => decrementLimit("child")}/>
-          <p> {children} Children(0-12) </p>
-          <AiFillPlusCircle style={{fontSize: "180%"}} onClick={() => setChildren(prev => prev + 1)}/>
-        </section>
-      </div>
+        <div className="search__passengers">
+          <p> Passengers: </p>
+          <section className="search__passengers__section">
+            <AiFillMinusCircle style={{fontSize: "180%"}} onClick={() => decrementLimit("adult")}/>
+            <p> {adults} Adults(12+) </p>
+            <AiFillPlusCircle style={{fontSize: "180%"}} onClick={() => setAdults(prev => prev + 1)}/>
+          </section>
+          <section className="search__passengers__section">
+            <AiFillMinusCircle style={{fontSize: "180%"}} onClick={() => decrementLimit("child")}/>
+            <p> {children} Children(0-12) </p>
+            <AiFillPlusCircle style={{fontSize: "180%"}} onClick={() => setChildren(prev => prev + 1)}/>
+          </section>
+        </div>
 
-      <div className="search__box">
-          <p> Price range: </p>
-          <RangeSlider labelAlwaysOn min={1} max={5000} thumbSize={27} marks={marks} value={price} onChange={setPrice}/>
-      </div>
+        <div className="search__box">
+            <p> Price range: </p>
+            <RangeSlider labelAlwaysOn min={1} max={5000} thumbSize={27} marks={marks} value={price} onChange={setPrice} color="white"/>
+        </div>
 
-      <Button color="grape" radius="md" size="lg"> Submit </Button>
-    </div>
+        <Button color="grape" radius="md" size="lg"> Submit </Button>
+      </div>
+    </MantineProvider>
   );
 }
 
